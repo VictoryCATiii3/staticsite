12adfe8aff8f8141e9c50bd7b0b4e08f5d7a4fe8
VictoryCATiii3 <victor7670236@gmail.com>
2025-01-30 19:45:49 -0800

    Added unit test and styles.css

---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_url(self):
+        node = TextNode("This is another text node", TextType.ITALIC)
+        node2 = TextNode("This is another text node", TextType.ITALIC, None)
+        self.assertEqual(node, node2)
+
+    def test_not_eq(self):
+        node = TextNode("These should be different", TextType.NORMAL, "A_URL")
+        node2 = TextNode("These should be different", TextType.IMAGE, "A_URL")
+        self.assertNotEqual(node, node2)
+
+    def test_url_eq(self):
+        node = TextNode("Some text here", TextType.CODE, "Same_URL")
+        node2 = TextNode("Some text here", TextType.CODE, "Same_URL")
+        self.assertEqual(node, node2)
+
+if __name__=="__main__":
+    unittest.main()
